72ba3c1edc02d0df300b1b942352da7edb0b3282
Learning Journal <pandey.kumar.prashant@gmail.com>
2020-06-12 14:48:10 +0530

    Initial Commit

---
+++ 02-HelloRDD/HelloRDD.py
@@ -0,0 +1,33 @@
+import sys
+
+from pyspark import SparkConf, SparkContext
+from collections import namedtuple
+
+from pyspark.sql import SparkSession
+
+from lib.logger import Log4j
+
+SurveyRecord = namedtuple("SurveyRecord", ["Age", "Gender", "Country", "State"])
+
+if __name__ == "__main__":
+    conf = SparkConf() \
+        .setMaster("local[3]") \
+        .setAppName("HelloRDD")
+
+    # sc = SparkContext(conf=conf)
+    spark = SparkSession.builder.config(conf=conf).getOrCreate()
+    sc = spark.sparkContext
+    logger = Log4j(spark)
+
+    linesRDD = sc.textFile(sys.argv[1])
+    partitionedRDD = linesRDD.repartition(2)
+
+    colsRDD = partitionedRDD.map(lambda line: line.replace('"', '').split(","))
+    selectRDD = colsRDD.map(lambda cols: SurveyRecord(int(cols[1]), cols[2], cols[3], cols[4]))
+    filteredRDD = selectRDD.filter(lambda r: r.Age < 40)
+    kvRDD = filteredRDD.map(lambda r: (r.Country, 1))
+    countRDD = kvRDD.reduceByKey(lambda v1, v2: v1 + v2)
+
+    colsList = countRDD.collect()
+    for x in colsList:
+        logger.info(x)
